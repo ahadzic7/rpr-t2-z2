468244cf4123a27e75e6266b0973c2dfb3897356
armin <ahadzic7@etf.unsa.ba>
2020-11-06 12:50:51 +0100

    Kreiran konstruktor koji prima cetiri parametra

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,18 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    private double donjaGranica, gornjaGranica;
+    private boolean donjaUkljucena, gornjaUkljucena;
+    private final static double EPS = 1e-6;
+
+    public Interval(double donjaGranica, double gornjaGranica, boolean donjaUkljucena, boolean gornjUkljucena) {
+        if (donjaGranica > gornjaGranica)
+            throw new IllegalArgumentException("Neispravne granice");
+        this.donjaGranica = donjaGranica;
+        this.gornjaGranica = gornjaGranica;
+        this.donjaUkljucena = donjaUkljucena;
+        this.gornjaUkljucena = gornjUkljucena;
+    }
 
 
 }
